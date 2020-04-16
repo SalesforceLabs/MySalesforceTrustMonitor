87d80151a7d004562cf20199806d367ffd44936f
Avijeet Singh <avitomar99@gmail.com>
2020-04-16 10:52:49 -0400

    Adding conditional styling for service status components

--- force-app/main/default/lwc/trustService/trustService.js
+++ force-app/main/default/lwc/trustService/trustService.js
@@ -1,5 +1,10 @@
-import { LightningElement,api } from 'lwc';
+import { LightningElement,api, track } from 'lwc';
 
 export default class TrustService extends LightningElement {
     @api services;
+    @api instance;
+
+    get bannerClassName(){
+        return this.instance.statusVariant+'BannerClass';
+    }
 }
