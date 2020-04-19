6d2ef204cfc023f564744603c0d4e82a70b30257
Avijeet Singh <avitomar99@gmail.com>
2020-04-19 00:24:17 -0400

    Enabled Mark as favorite feature to save the search instance key , committed duplicate rules and a tab to show the object

--- force-app/main/default/lwc/instanceSearchResultTile/instanceSearchResultTile.js
+++ force-app/main/default/lwc/instanceSearchResultTile/instanceSearchResultTile.js
@@ -1,7 +1,15 @@
 import { LightningElement, api, wire } from 'lwc';
-
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { createRecord } from 'lightning/uiRecordApi';
+import { reduceErrors } from 'c/ldsUtils';
+import SFINSTANCE_OBJECT from '@salesforce/schema/SFInstance__c';
+import NAME_FIELD from '@salesforce/schema/SFInstance__c.Name';
+import INSTANCE_KEY_TYPE__c_FIELD from '@salesforce/schema/SFInstance__c.Instance_Key_Type__c';
+import OWNERID_FIELD from '@salesforce/schema/SFInstance__c.OwnerId';
+import ALIAS__c_FIELD from '@salesforce/schema/SFInstance__c.Alias__c';
 import { NavigationMixin,CurrentPageReference } from 'lightning/navigation';
 import { fireEvent} from 'c/pubsub';
+import Id from '@salesforce/user/Id';
 
 export default class InstanceSearchResultTile extends LightningElement {
     @api keyx;
@@ -13,6 +21,7 @@ export default class InstanceSearchResultTile extends LightningElement {
     @api instanceKey;
     @api aliasType;
     @api isInstance;
+    sfInstance__cId;
 
     @wire(CurrentPageReference) pageRef;
 
@@ -38,5 +47,35 @@ export default class InstanceSearchResultTile extends LightningElement {
         eventData.keyType = event.target.dataset.keytype;       
         console.log(event.target.dataset.instancekey+JSON.stringify(eventData));
         fireEvent(this.pageRef,'getStatus',eventData);
+    }
+
+    saveFavorite(event){
+        event.preventDefault();
+        const fields = {};
+        fields[OWNERID_FIELD.fieldApiName] = Id;
+        fields[NAME_FIELD.fieldApiName] = event.target.dataset.instancekey;
+        fields[INSTANCE_KEY_TYPE__c_FIELD.fieldApiName] = event.target.dataset.keytype; 
+        fields[ALIAS__c_FIELD.fieldApiName] = event.target.dataset.alias;         
+        const recordInput = { apiName: SFINSTANCE_OBJECT.objectApiName, fields };
+        createRecord(recordInput)
+            .then((SFInstance__c) => {
+                this.sfInstance__cId = SFInstance__c.id;
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Marked as Favorite !',
+                        variant: 'success'
+                    })
+                );
+            })
+            .catch((error) => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error',
+                        message: 'Instance key already exists !',
+                        variant: 'error'
+                    })
+                );
+            });
     }
 }
