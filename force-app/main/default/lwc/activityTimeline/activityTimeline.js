3da1fb6886bf02e15f15752a5fa3e407170cdec1
Avijeet Singh <avitomar99@gmail.com>
2020-04-21 12:29:25 -0400

    Fixing Incident & Maintenance Components

--- force-app/main/default/lwc/activityTimeline/activityTimeline.js
+++ force-app/main/default/lwc/activityTimeline/activityTimeline.js
@@ -3,6 +3,8 @@ import { LightningElement, api } from 'lwc';
 export default class ActivityTimeline extends LightningElement {
     @api maintenances;
     @api incidents;
+    @api showMaintenances;
+    @api showIncidents;
 
     get incidentExists(){
         if(typeof this.incidents === "undefined"){
@@ -12,6 +14,17 @@ export default class ActivityTimeline extends LightningElement {
             return true;
         }
         else if (this.incidents.length === 0) {
+            return false;
+        }
+    }
+    get maintenanceExists(){
+        if(typeof this.maintenances === "undefined"){
+            return false;
+        }
+        else if(this.maintenances.length > 0){
+            return true;
+        }
+        else if (this.maintenances.length === 0) {
             return false;
         }
     }
